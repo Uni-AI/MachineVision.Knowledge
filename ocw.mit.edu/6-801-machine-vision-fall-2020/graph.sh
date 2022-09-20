746165fd92271abd1e9bacf2fc10af2332a3af15
Twin-Flame <61005029+Twin-Flame@users.noreply.github.com>
2022-09-20 19:18:47 -0400

    Create graph.sh

---
+++ ocw.mit.edu/6-801-machine-vision-fall-2020/graph.sh
@@ -0,0 +1,32 @@
+course="Machine Vision, Fall 2020, Prof. Berthold Horn"
+school="MIT"
+url="https://ocw.mit.edu/courses/6-801-machine-vision-fall-2020/"
+videos="https://ocw.mit.edu/courses/6-801-machine-vision-fall-2020/video_galleries/lecture-videos/"
+
+download(){
+youtube-dl https://youtu.be/tY2gczObpfU \
+https://youtu.be/uM5AoUgCA00 \
+https://youtu.be/t9w_WuyVJ-A \
+https://youtu.be/3NarS3QpaU0 \
+https://youtu.be/SSlPV1iYjHw \
+https://youtu.be/OOrT17RsVxE \
+https://youtu.be/qqtEQsBjh28 \
+https://youtu.be/LP7z6vVmnms \
+https://youtu.be/J4gbaRyWpoM \
+https://youtu.be/XSUjp-cDyeY \
+https://youtu.be/kjCQ2DUn55Y
+# 11
+youtube-dl https://youtu.be/jLvgNmGuxGk \
+https://youtu.be/lVfm6C51t_Y \
+https://youtu.be/6SvDRpQeZrA \
+https://youtu.be/R1yLwFOfwwc \
+https://youtu.be/tblzZhYZtvU \
+https://youtu.be/R8O0vlHBeNc \
+https://youtu.be/uiG6KY0Cqd8 \
+https://youtu.be/kigLhcTm_gg \
+https://youtu.be/Mcy-LlcD8jI \
+# 21
+youtube-dl https://youtu.be/-n0cXgPSwOw \
+https://youtu.be/x4F1oqm7ETk \
+https://youtu.be/s9wu_z8EKAc
+}
